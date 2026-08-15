69a5c06205953a5f83e57d474e9b4781c53d9482
Farhan0386 <farhan03860@gmail.com>
2026-08-15 09:22:57 +0530

    fs module completed in web dev 3 unit 1

--- 2nd Year/Web-Dev-3/KRMU-WEB-DEV-3-main/FS Module/src/index.js
+++ 2nd Year/Web-Dev-3/KRMU-WEB-DEV-3-main/FS Module/src/index.js
@@ -1,51 +1,15 @@
-// src/index.js
+const fs = require("fs");
+const path = require("path");
 
-const fs = require('fs'); // Import the file system module
- 
-function processStudent(student) {
-    const marks = parseInt(student.split(' ')[1]);
-    
-    if (marks >= 40) {
-        return 'PASS';
-    } else {
-        return 'FAIL';
-    }
-}
- 
-function writeReport(studentsFilePath, reportFilePath) { 
- 
-    fs.readFile(studentsFilePath, 'utf8', function(err, data) {
-        if (err) {
-            console.error('Error reading file:', err);
-            return;
-        }
-         
-        const students = data.split('\n');
-         
-
-        let reportContent = '';
- 
-        for (const student of students) {
-            
-            // Split the name and marks
-            const [name, marks] = student.trim().split(/\s+/); 
-
-            const result = processStudent(student);
-
-            reportContent += `${name} - ${result}\n`;         
-        }
-         
-        fs.writeFile(reportFilePath, reportContent, function(err) {
-            if (err) {
-                console.error('Error writing file:', err);
-                return;
-            }
-
-            console.log(`Report written to '${reportFilePath}'`);
-        });
-    });   
-}
 const inputPath = path.join(__dirname, "students.txt");
 const outputPath = path.join(__dirname, "report.txt");
 
-writeReport(inputPath, outputPath);
+const data = fs.readFileSync(inputPath, "utf8").split("\n")
+
+ let result= data.map((string)=>
+{
+    const[name,marks]= string.split(" ")
+    return `${name} - ${marks>=40 ? "PASS" : "FAIL"}`
+})
+
+fs.writeFileSync(outputPath, result.join("\n"));
